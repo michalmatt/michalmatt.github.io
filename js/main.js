4a450dff193164e79b35d73a7b5bd087ab3eb4d4
Michal Matt <michu.em@gmail.com>
2022-01-16 00:16:01 +0100

    adding very very simple lightbox for portfolio review and animations for portfolio filtering

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $(function(){
     
     //CSS opacity change on hover
     $(".image-box img").mouseover(function(){
-        $(this).css("opacity", 0.6);
+        $(this).css("opacity", 0.8);
     });
     $(".image-box img").mouseout(function(){
         $(this).css("opacity", 1);
@@ -59,13 +59,68 @@ $(function(){
     //portfolio filtering function
     var $btns = $('.filter-btn').click(function() {
         if (this.id == 'all') {
-            $('.portfolio-content > div').show();
+            $('.portfolio-content > div').show("slow");
         } else {
-            var $el = $('.' + this.id).show();
-            $('.portfolio-content > div').not($el).hide();
+            var $el = $('.' + this.id).show("slow");
+            $('.portfolio-content > div').not($el).hide("slow");
         }
         $btns.removeClass('active');
         $(this).addClass('active');
     })
+    
+    //The Modal/Lightbox related code
+    
+    // Open the Modal
+    function openModal() {
+        document.getElementById("myModal").style.display = "block";
+    }
+
+    // Close the Modal
+    function closeModal() {
+        document.getElementById("myModal").style.display = "none";
+    }
+
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var captionText = document.getElementById("caption");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+    }
+    
+    $(".image-box").click(function(){
+        var imgIdNumber = parseInt($(this).attr("id"), 10);
+        openModal();
+        currentSlide(imgIdNumber);
+    });
+    
+    $(".close").click(function(){
+        closeModal();
+    });
+    
+    $(".next").click(function(){
+        plusSlides(1);
+    });
+    
+    $(".prev").click(function(){
+        plusSlides(-1);
+    });
+    
+    var slideIndex = 1;
+    showSlides(slideIndex);
     
 });
